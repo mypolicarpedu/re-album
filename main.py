b93a6c12d12dfd0c3171b3f75fef649fb0103917
mypolicarpedu <mypolicarp@vt.edu>
2022-04-04 18:18:09 -0400

    Loading data test

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
+
+def load_data():
+    print("Loading data!")
+    hello = [("Hello", 10), ("I am bigger", 1500)]
+
+    return hello
+
+
 #main function for project functionality
 def main():
     print("This is the machine learning project!")
+    hello = load_data()
+    print(hello)
   
   
 
