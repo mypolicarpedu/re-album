85edf60e0d727a5d751a0cbf8a8d27c1cbca9177
mypolicarpedu <mypolicarp@vt.edu>
2022-05-04 17:10:27 -0400

    Cleaned up code and added average functionality

--- main.py
+++ main.py
@@ -41,11 +41,11 @@ import plotly.express as px
 
 
 def load_data():
-    print("Loading data!")
+    print("Loading data from spotify_dataset.csv!")
+    print("--------------------------------------")
 
     #load data from desired data set (provided in the github)
     spotify_data = pd.read_csv('./spotify_dataset.csv')
-
 
     return spotify_data
 
@@ -79,7 +79,12 @@ def main():
     #an additional example of features not used
     #data.drop(['track', 'artist', 'uri', 'mode','decade','key','time_signature', 'sections', 'popularity'], axis=1, inplace=True)
     data.drop(['track', 'artist', 'uri', 'mode','decade'], axis=1, inplace=True)
+
+    #clean printouts for user to know what they are working with
+    print("------------------------")
+    print("Data shape: ")
     print(data.shape)
+    print("Total data features selected for similarity check: ")
     print(data)
 
     #embedding with 2 dimensions
@@ -103,20 +108,24 @@ def main():
     fig = px.scatter(newproj, x='x', y='y', hover_data=['x','y','track'])
     fig.show()
 
-    #testa result value to see if its x,y fitted
-    print(tsne_result[1])
+    #test a result value to see if its x,y fitted properly
+    #print(tsne_result[1])
 
     #different songs and how to fetch their points
     #point = fetch_song_point(searchcopy, 'While I Was Walking', 'Tommy McCook', tsne_result) 
     point = fetch_song_point(searchcopy, 'Mashed Potato Time', 'Dee Dee Sharp', tsne_result)
-    #point = fetch_song_point(searchcopy, 'No Tears Left To Cry', 'Ariana Grande', tsne_result)
+
+    #point2 = fetch_song_point(searchcopy, 'No Tears Left To Cry', 'Ariana Grande', tsne_result)
 
     #invaid song test that throws error
-    error_point = fetch_song_point(searchcopy, 'Invalid Name', 'Invalid Name', tsne_result)
+    #error_point = fetch_song_point(searchcopy, 'Invalid Name', 'Invalid Name', tsne_result)
 
-
-    print("song tested")
     #point = [(24,24)]
+    print("Point fetched to cater recommandations to: " + str(point))
+
+    song_test(proj, labelcopy, printoutcopy, point)
+
+    """
     distances = cdist(point, proj, 'euclidean')
     print(distances)
     index = np.argsort(distances[0])
@@ -131,6 +140,9 @@ def main():
 
     print(labelcopy.loc[shortened])
     print(printoutcopy.loc[shortened])
+    """
+
+    
 
     #song_test(proj, labelcopy, printoutcopy)
     #distances = cdist
@@ -141,45 +153,73 @@ def main():
 
 #test the song and find 10 songs most closest related to it based off features
 def song_test(input_data,label_data,print_data,point):
-    print("song tested")
 
-    #test point value
-    #point = [(24,24)]
-
-
-    distances = cdist(point, input_data, 'cosine')
-    print(distances)
+    #choose distance algorithm here - euclidean, cosine, etc
+    distances = cdist(point, input_data, 'euclidean')
+    #print(distances)
+    
+    #sort distances from least to greatest index
     index = np.argsort(distances[0])
-    print(index)
-    print(index[:10])
+    #print(index)
+    #print(index[:10])
 
+    #shorten distances to top 10 indexes - change 10 to wharever n number of recommandations
     shortened = index[:10]
-
-    print_data['distance'] = distances.tolist()[0]
     #shortened = index[:10]
     #print(shortened, 100)
 
+    #append distance data to the list for relevant output
+    print_data['distance'] = distances.tolist()[0]
+
+    print("Recommendation album - including the input song:")
+    print("------------------------------------------------")
+    #to pick a different printout type with just labels
     #print(label_data.loc[shortened])
     print(print_data.loc[shortened])
 
 #fetch the song location from all the points in the set for usage in similarity checks
 def fetch_song_point(label_data, songname, artistname, tsne_result):
-    print("fetching song point")
+    print("Looking for song in data set to fetch data point...")
     if np.where(label_data['track'] == songname):
+
         if np.where(label_data['artist'] == artistname):
-            print("song found!")
+
             temp = np.where((label_data['track'] == songname) & (label_data['artist'] == artistname))
-            print(temp[0])
+
+            #print found index
+            #print(temp[0])
+
             result = tsne_result[temp[0]]
 
             if len(result) == 0:
-                print("no song found in set...")
+                print("Song not found in set!")
                 return "not found"
-                #return("Error")
+            else:
+                print("Song found in set!")
             print(result)
             
             return result
     
+#function to average out song pointss
+def average_song_points(point_list):
+    print("Averaging song points!")
+    sumx = 0
+    sumy = 0
+    count = 0
+
+    for point in point_list:
+        sumx = sumx + point[0]
+        sumy = sumy + point[1]
+        count = count + 1
+    
+    output = tuple(sumx/count,sumy/count)
+
+    outputlist = [output]
+
+    return output
+
+
+
 
 
   
